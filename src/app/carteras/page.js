fb48e20262824cce10ad7a055b28695984dc3fd2
Jesii-22 <jesimarzeniuk@gmail.com>
2024-10-03 20:47:59 -0300

    cf

--- src/app/carteras/page.js
+++ src/app/carteras/page.js
@@ -1,35 +1,13 @@
 import ProductCard from '@/app/components/ProductCard';
 import mongoose from 'mongoose';
-
-const connectMongo = async () => {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  }
-};
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-});
-
-const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
-
-const getAllProductsDB = async () => {
-  await connectMongo();
-  const products = await Product.find({});
-  return products;
-};
+import { getAllProductsDB } from '../actions';
 
 const Carteras = async () => {
-  const products = await getAllProductsDB();  
+  const response = await getAllProductsDB();  
 
-  console.log(products); 
+  console.log(response.products); 
   
-  if (!products || products.length === 0) {
+  if (!response.products || response.products.length === 0) {
     return <h1>No se encontraron productos</h1>;
   }
 
@@ -37,7 +15,7 @@ const Carteras = async () => {
     <div>
       <h1 className="text-5xl font-bold font-serif my-10 text-center title-effect">Galería de Carteras</h1>
       <div id="cambioborde" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {products.map((product, index) => {
+        {response.products.map((product, index) => {
           const imageName = product.image ? product.image : `cartera.${(index % 8) + 1}.png`;
 
           return (
